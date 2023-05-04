f8cf9d70b8b48218f1b0f9322a81a0bc35e53ce6
Andreas Auerswald <andreas.auerswald@aplixa.de>
2023-05-04 19:24:42 +0200

    PDFtoJPEG as typescript

---
+++ PDFEdit/wwwroot/ts/pdfUtils.ts
@@ -0,0 +1,21 @@
+namespace org.site {
+
+    declare var pdfjsLib: any;
+
+    export async function PDFtoJPEG(bytes: any, pageIdx: any): Promise<string> {
+        let canvas = document.createElement("canvas");
+
+        let pdf = await pdfjsLib.getDocument({ data: bytes }).promise
+        let page = await pdf.getPage(pageIdx + 1);
+        let viewport = page.getViewport({ scale: 1 });
+
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+        let ctx = canvas.getContext('2d');
+        let renderContext = { canvasContext: ctx, viewport: viewport };
+
+        await page.render(renderContext).promise
+        return canvas.toDataURL();
+    }
+}
