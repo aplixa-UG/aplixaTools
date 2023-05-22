d4bd4f19c0c924a8fb125143e486651c0b223e9f
BlackBird <93947677+BlackBirdTV@users.noreply.github.com>
2023-05-22 15:56:11 +0200

    Added Nav Menu functionallity

---
+++ AplixaTools.PDFEdit/Components/NavItem.razor.cs
@@ -0,0 +1,20 @@
+﻿using AplixaTools.PDFEdit.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace AplixaTools.PDFEdit.Components;
+
+public partial class NavItem
+{
+	[Inject] public NavigationManager NavigationManager { get; set; }
+
+	[Parameter] public string Title { get; set; }
+	[Parameter] public string Href { get; set; }
+	[Parameter] public string Icon { get; set; }
+	[Parameter] public EventCallback OnNavigated { get; set; }
+
+	private async Task Navigate()
+	{
+		NavigationManager.NavigateTo(Href);
+		await OnNavigated.InvokeAsync();
+	}
+}
---
+++ AplixaTools.PDFEdit/Components/NavMenu.razor.cs
@@ -0,0 +1,16 @@
+﻿namespace AplixaTools.PDFEdit.Components;
+
+public partial class NavMenu
+{
+	private bool _showNavMenu = false;
+
+	private void ToggleNavMenu()
+	{
+		_showNavMenu = !_showNavMenu;
+	}
+
+	private void HideNavMenu()
+	{
+		_showNavMenu = false;
+	}
+}
