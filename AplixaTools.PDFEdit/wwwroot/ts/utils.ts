2d059f7205be146f22892bc129a4ae118d98cacc
Luis Weinzierl <luis.weinzierl@aplixa.de>
2023-06-08 13:59:04 +0200

    Removed debug console.logs

--- AplixaTools.PDFEdit/wwwroot/ts/utils.ts
+++ AplixaTools.PDFEdit/wwwroot/ts/utils.ts
@@ -38,13 +38,8 @@
 
         var rot = parseFloat(elem.firstElementChild.getAttribute("rotation"));
 
-        console.log(elem.firstElementChild);
-        console.log(elem.firstElementChild.getAttribute("rotation"));
-        console.log(rot + "deg")
-
         if (rot == 90 || rot == 270) {
             var topOffset = (elemRect.height - elemRect.width) / 2;
-            console.log([x - topOffset, y + topOffset, elemRect.height, elemRect.width]);
             return [x - topOffset, y + topOffset, elemRect.height, elemRect.width];
         }
         return [x, y, elemRect.width, elemRect.height];
